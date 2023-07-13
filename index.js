601ee10c62df3cfcf8f787c7ba0e936b80f1415b
BillyBooglyBob <123722533+BillyBooglyBob@users.noreply.github.com>
2023-07-14 00:40:02 +1000

    Allowed dimension of the grid to be changed

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
 const grid = document.querySelector('.grid');
 
-let dimension = 10;
-for (let i = 0; i < dimension; i++) {
-    const newCol = document.createElement('div');
-    newCol.setAttribute('style', 
-        `flex-grow: 1;
-        display: flex;
-        flex-direction: column;`)
-    for (let j = 0; j < dimension; j++) {
-        const newDiv = document.createElement('div');
-        newDiv.setAttribute('style', 
-            `flex: auto; 
-            height: 20px;
-            border: 1px solid black`)
-        newDiv.addEventListener('mouseover', changeColour);
-        newCol.appendChild(newDiv);
+function getDimension() {
+    let dimension = prompt('Enter new dimension');
+    if (dimension > 100) {
+        return getDimension();
     }
-    grid.appendChild(newCol);
+    return dimension;
 }
+
+function createGrid(dimension) {
+    for (let i = 0; i < dimension; i++) {
+        const newCol = document.createElement('div');
+        newCol.setAttribute('style', 
+            `flex-grow: 1;
+            display: flex;
+            flex-direction: column;`)
+        for (let j = 0; j < dimension; j++) {
+            const newDiv = document.createElement('div');
+            newDiv.setAttribute('style', 
+                `flex: auto; 
+                height: 20px;
+                border: 1px solid black`)
+            newDiv.addEventListener('mouseover', changeColour);
+            newCol.appendChild(newDiv);
+        }
+        grid.appendChild(newCol);
+    } 
+}
+
+function changeGrid() {
+    let dimension = getDimension();
+    grid.replaceChildren();
+    createGrid(dimension);
+}
+
+
 
 function changeColour(e) {
     e.target.style.background = 'black';
 }
+
+createGrid(16);
+
+const btn = document.querySelector('.set-dimension');
+btn.addEventListener('click', changeGrid);
